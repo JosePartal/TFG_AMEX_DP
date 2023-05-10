dd428a48deab0fba7fac8c0f20c1443362670508
Lyapunov230 <jpartal@ucm.es>
2023-05-10 21:00:15 +0200

    dummy changes and mean function V1

--- feature_engineering.py
+++ feature_engineering.py
@@ -62,19 +62,35 @@ If we dummy encode them we will have k-1 new features for each categorical featu
 so there will be less features in the final dataset and the dimensionality will be reduced.
 """
 
-# Concatenar train y test y hacer el dummy encoding, luego separarlos
-def dummy_encoding(train_df, test_df, cat_features): # Quitar test, dejar solo un df y hacer 2 llamadas
+# def dummy_encoding_2(train_df, test_df, cat_features): 
+#     # Dummy encoding
+#     train_df_oh = pd.get_dummies(train_df, columns = cat_features, drop_first = True)
+#     test_df_oh = pd.get_dummies(test_df, columns = cat_features, drop_first = True)
+
+#     # List of the new dummy encoded features
+#     dummies_train = list(set(train_df_oh.columns) - set(train_df.columns))
+#     dummies_test = list(set(test_df_oh.columns) - set(test_df.columns))
+
+
+#     return train_df_oh, test_df_oh, dummies_train, dummies_test
+
+# Join train and test to perform dummy encoding and then separate them again
+def dummy_encoding(train_df, test_df, cat_features):
+    # Join train and test
+    df = pd.concat([train_df, test_df], axis = 0)
+
     # Dummy encoding
-    train_df_oh = pd.get_dummies(train_df, columns = cat_features, drop_first = True)
-    test_df_oh = pd.get_dummies(test_df, columns = cat_features, drop_first = True)
+    df_oh = pd.get_dummies(df, columns = cat_features, drop_first = True)
+
+    # Separate train and test
+    train_df_oh = df_oh.iloc[:train_df.shape[0], :]
+    test_df_oh = df_oh.iloc[train_df.shape[0]:, :]
 
     # List of the new dummy encoded features
     dummies_train = list(set(train_df_oh.columns) - set(train_df.columns))
     dummies_test = list(set(test_df_oh.columns) - set(test_df.columns))
 
-
     return train_df_oh, test_df_oh, dummies_train, dummies_test
-
 
 
 # In[5]: Feature engineering functions III: Aggregations
@@ -148,7 +164,7 @@ def feat_lag(data, lags: list): # [1, 2, 3, 6, 11]
         # Iterate through each lag
         for lag in lags:
             # Check if lag < num_observations
-            if lag < num_observations: 
+            if lag < num_observations: # REVISAR Pasar a NaN las observaciones que no existan
                 # Get lag observation
                 lag_observation = sorted_data.iloc[lag]
                 # Build dictionary to append to list
@@ -159,11 +175,138 @@ def feat_lag(data, lags: list): # [1, 2, 3, 6, 11]
                     if column not in ['customer_ID', 'S_2']:
                         lag_variable['lag_{}_{}'.format(lag, column)] = lag_observation[column]
                 lag_variables.append(lag_variable)
+            # Else fill with NaNs
+            else:
+                # Build dictionary to append to list
+                lag_variable = {'customer_ID': customer_id, 'lag': lag}
+
+                # Iterate through each column of the data (except customer_ID and S_2)
+                for column in cid_data.columns:
+                    if column not in ['customer_ID', 'S_2']:
+                        lag_variable['lag_{}_{}'.format(lag, column)] = np.nan
+                lag_variables.append(lag_variable)
 
     # Convertir la lista de diccionarios en un DataFrame
     lag_data = pd.DataFrame(lag_variables)
 
     return lag_data
+
+
+# In[8]: Feature engineering functions VI: Period means
+
+# Function that creates new features with the mean of a given variable for a given period
+# depending on the number of observations of each customer
+
+def feat_period_means(data, features): # features = numerical_features
+    period_means = []
+    customer_ids = []
+    column_names = []
+    for customer_id, cid_data in data.groupby(['customer_ID']):
+        # We are going to calculate the mean of different groups of observations. If a customer
+        # does not have enough observations, we will fill with NaNs.
+
+        # Get the mean of the last 6 observations and the mean of the 3 observations before the last 3 observations
+        if len(cid_data) >= 6:
+            mean_last_6M = cid_data[features].iloc[-6:].mean().values.astype(np.float32)
+            mean_before_3M = cid_data[features].iloc[-6:-3].mean().values.astype(np.float32)
+            # Append column names
+            column_names.extend([col + '_mean_last_6M' for col in features])
+            column_names.extend([col + '_mean_before_3M' for col in features])
+        else:
+            mean_last_6M = np.full(len(features), np.nan)
+            mean_before_3M = np.full(len(features), np.nan)
+            # Append column names
+            column_names.extend([col + '_mean_last_6M' for col in features])
+            column_names.extend([col + '_mean_before_3M' for col in features])
+        # Append to lists
+        period_means.append(mean_last_6M)
+        period_means.append(mean_before_3M)
+        
+        # Get the mean of the last 3 observations
+        if len(cid_data) >= 3:
+            mean_last_3M = cid_data[features].iloc[-3:].mean().values.astype(np.float32)
+            # Append column names
+            column_names.extend([col + '_mean_last_3M' for col in features])
+        else:
+            mean_last_3M = np.full(len(features), np.nan)
+            # Append column names
+            column_names.extend([col + '_mean_last_3M' for col in features])
+        # Append to lists
+        period_means.append(mean_last_3M)
+
+        # Get the mean of the 6 observations before the last 6 observations
+        if len(cid_data) >= 12:
+            mean_before_6M = cid_data[features].iloc[-12:-6].mean().values.astype(np.float32)
+            # Append column names
+            column_names.extend([col + '_mean_before_6M' for col in features])
+        else:
+            mean_before_6M = np.full(len(features), np.nan)
+            # Append column names
+            column_names.extend([col + '_mean_before_6M' for col in features])
+        # Append to lists
+        period_means.append(mean_before_6M)
+
+        # Now we compute the relative increments of the means
+        # Relative semestral growth
+        inc_12m_6m = (mean_last_6M - mean_before_6M) / mean_before_6M
+        # Relative trimestral growth
+        inc_3m_3m = (mean_last_3M - mean_before_3M) / mean_before_3M
+
+        # Append column names
+        column_names.extend([col + '_inc_12m_6m' for col in features])
+        column_names.extend([col + '_inc_3m_3m' for col in features])
+
+        # Append to lists
+        period_means.append(inc_12m_6m)
+        period_means.append(inc_3m_3m)
+        customer_ids.append(customer_id)
+
+    # Concatenate
+    period_means = np.concatenate(period_means, axis=0)
+    # Transform to dataframe
+    period_means = pd.DataFrame(period_means, columns=column_names)
+
+    # Add customer id
+    period_means['customer_ID'] = customer_ids
+
+    return period_means
+
+
+
+
+
+
+
+
+
+# In[9]: Feature engineering functions VII: First/last observations
+
+# Function that creates new features with the first and last observations of a given variable
+# 1. Last - First: The change since we first see the client to the last time we see the client.
+# 2. Last / First: The fractional difference since we first see the client to the last time we see the client.
+
+def feat_last_diffdiv(agg_features): # df_num_agg
+    # Iterate through features
+    for feature in agg_features:
+
+        # Check if last and first are in the feature name and compute difference and division
+        if 'last' in feature and feature.replace('last', 'first') in agg_features:
+            agg_features[feature + '_lag_sub_first'] = agg_features[feature] - agg_features[feature.replace('last', 'first')]
+            agg_features[feature + '_lag_div_first'] = agg_features[feature] / agg_features[feature.replace('last', 'first')]
+
+        # Check if last and mean are in the feature name and compute difference and division
+        elif 'last' in feature and feature.replace('last', 'mean') in agg_features:
+            agg_features[feature + '_lag_sub_mean'] = agg_features[feature] - agg_features[feature.replace('last', 'mean')]
+            agg_features[feature + '_lag_div_mean'] = agg_features[feature] / agg_features[feature.replace('last', 'mean')]
+
+    return agg_features
+
+
+# In[10]: Feature engineering functions VIII: Relative growth
+
+# Function that creates new features with the relative growth of a given variable. 
+# We will calculate the difference between the means of the first half and the second half of the observations of each customer.
+# e.g. (mean of the last 6 months - mean of the first 6 months) / mean of the first 6 months
 
 # %%
 # # In[6]: data
