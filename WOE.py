8e942855bddcd8593352ae8b7b500cfe95313c42
Lyapunov230 <jpartal@ucm.es>
2023-06-05 11:57:45 +0200

    commit test after url change

--- WOE.py
+++ WOE.py
@@ -40,6 +40,8 @@ train_labels, train, test = fe.load_datasets(oh=False)
 cat_features = ['B_30_last', 'B_30_first', 'B_38_last', 'B_38_first', 'D_63_last', 'D_63_first', 'D_64_last', 'D_64_first', 'D_66_last', 'D_66_first', 'D_68_last', 
                 'D_68_first', 'D_114_last', 'D_114_first', 'D_116_last', 'D_116_first', 'D_117_last', 'D_117_first', 'D_120_last', 'D_120_first', 'D_126_last', 'D_126_first']
 
+num_features = [col for col in train.columns if col not in cat_features and col != 'customer_ID']
+
 # Lista de variables (exlucyendo 'customer_ID)
 features = list(train.columns)
 features.remove('customer_ID')
@@ -85,4 +87,60 @@ def amex_metric(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> float:
 train_df = train.merge(train_labels, left_on='customer_ID', right_on='customer_ID')
 del train, train_labels
 
-# In[6]: 
+# In[6]: Cálculo del WOE
+
+# Función para calcular el WOE de una variable categórica
+def woe_cat(df, cat_var, target):
+    '''
+    Calcula el WOE de una variable categórica
+    '''
+    # Tabla de frecuencias
+    freq_table = pd.crosstab(df[cat_var], df[target], margins=True)
+    freq_table = freq_table.reset_index()
+    freq_table = freq_table.rename(columns={0: 'non_default', 1: 'default'})
+    freq_table['total'] = freq_table['non_default'] + freq_table['default']
+    freq_table['non_default_pct'] = freq_table['non_default'] / freq_table['total']
+    freq_table['default_pct'] = freq_table['default'] / freq_table['total']
+    freq_table['woe'] = np.log(freq_table['non_default_pct'] / freq_table['default_pct'])
+    freq_table['iv'] = (freq_table['non_default_pct'] - freq_table['default_pct']) * freq_table['woe']
+    freq_table['iv'] = freq_table['iv'].sum()
+    freq_table['variable'] = cat_var
+    freq_table = freq_table[['variable', cat_var, 'non_default', 'default', 'total', 'non_default_pct', 'default_pct', 'woe', 'iv']]
+    return freq_table
+
+# Función para calcular el WOE de una variable numérica
+def woe_num(df, num_var, target):
+    '''
+    Calcula el WOE de una variable numérica
+    '''
+    # Tabla de frecuencias
+    freq_table = pd.crosstab(df[num_var], df[target], margins=True)
+    freq_table = freq_table.reset_index()
+    freq_table = freq_table.rename(columns={0: 'non_default', 1: 'default'})
+    freq_table['total'] = freq_table['non_default'] + freq_table['default']
+    freq_table['non_default_pct'] = freq_table['non_default'] / freq_table['total']
+    freq_table['default_pct'] = freq_table['default'] / freq_table['total']
+    freq_table['woe'] = np.log(freq_table['non_default_pct'] / freq_table['default_pct'])
+    freq_table['iv'] = (freq_table['non_default_pct'] - freq_table['default_pct']) * freq_table['woe']
+    freq_table['iv'] = freq_table['iv'].sum()
+    freq_table['variable'] = num_var
+    freq_table = freq_table[['variable', num_var, 'non_default', 'default', 'total', 'non_default_pct', 'default_pct', 'woe', 'iv']]
+    return freq_table
+
+
+# In[7]: Cálculo del WOE de las variables
+
+# Cálculo del WOE de las variables categóricas
+woe_cat_df = pd.DataFrame()
+for cat_var in cat_features:
+    woe_cat_df = woe_cat_df.append(woe_cat(train_df, cat_var, 'target'))
+
+# Cálculo del WOE de las variables numéricas
+woe_num_df = pd.DataFrame()
+for num_var in num_features:
+    woe_num_df = woe_num_df.append(woe_num(train_df, num_var, 'target'))
+
+
+
+
+# %%
