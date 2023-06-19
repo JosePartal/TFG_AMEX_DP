ddb4e6a24d8363e6a98aa8617ac66223d917a43a
Lyapunov230 <jpartal@ucm.es>
2023-06-19 18:53:11 +0200

    test predictions

--- AMEX default prediction XGBoost.py
+++ AMEX default prediction XGBoost.py
@@ -134,6 +134,10 @@ def amex_metric_mod(y_true, y_pred):
 
 train_df_oh, test_df_oh, dummies_train, dummies_test = fe.dummy_encoding(train, test_data, cat_features)
 
+# Limpiamos la memoria
+del train, test_data, dummies_train, dummies_test, not_used, bin_features_1, bin_features_2, bin_features_3, num_features
+gc.collect()
+
 
 # In[6]: Separamos los datos en entrenamiento y test
 
@@ -195,13 +199,20 @@ for fold, (train_index, valid_index) in enumerate(split):
     # Creamos el dataset de entrenamiento indicando las variables categóricas
     # (Cambiar a DeviceQuantileDMatrix, es mucho más rápido)
 
-    dtrain = xgb.DeviceQuantileDMatrix(X_train, label=y_train, feature_names=X_train.columns, nthread=-1, enable_categorical=True)
+    dtrain = xgb.QuantileDMatrix(X_train, label=y_train, feature_names=X_train.columns, nthread=-1, enable_categorical=True)
     dvalid = xgb.DMatrix(X_valid, label=y_valid, feature_names=X_valid.columns, nthread=-1, enable_categorical=True)
+
+    # Medimos el tiempo de entrenamiento para cada fold: start
+    start = time.time()
 
     # Entrenamos el modelo para el fold actual
 
     xgb_model = xgb.train(xgb_parms, dtrain, num_boost_round=2500, evals=[(dtrain,'train'),(dvalid,'test')],
                             early_stopping_rounds=50, verbose_eval=50) # feval ver custom metric https://www.kaggle.com/code/jiweiliu/rapids-cudf-feature-engineering-xgb
+    
+    # Medimos el tiempo de entrenamiento para cada fold: end
+    end = time.time()
+    print(f'Tiempo de entrenamiento para el fold {fold}:', end - start)
     
     # Guardamos el modelo
     fe.save_model_fe('XGBoost', xgb_model, fold, current_time) # Crea una carpeta para cada minuto, cambiar para que lo guarde todo en la misma
@@ -214,8 +225,21 @@ for fold, (train_index, valid_index) in enumerate(split):
     
     # Calculamos el score para el fold actual con la métrica customizada
     AMEX_score = amex_metric_mod(y_valid.values, y_pred) # DA ERROR LA ORIGINAL
-    print('Métrica de Kaggle para el fold {fold}:', AMEX_score)
+    print(f'Métrica de Kaggle para el fold {fold}:', AMEX_score)
     scores['AMEX'].append(AMEX_score)
+
+    # # Predicciones sobre el conjunto de test
+    # test_df_oh = test_df_oh.groupby('customer_ID').tail(1).set_index('customer_ID') # Última observación
+    # test_df_oh = test_df_oh.reset_index()
+    # X_test = test_df_oh.drop(columns=['customer_ID', 'S_2'])
+    # dtest = xgb.DMatrix(X_test, feature_names=X_test.columns, nthread=-1, enable_categorical=True)
+    # y_pred_test = xgb_model.predict(dtest)
+    # print(f'Prediction for fold {fold} done')
+    # # Creamos un dataframe con las predicciones
+    # submission = pd.DataFrame({'customer_ID': test_df_oh['customer_ID'], 'prediction': y_pred_test})
+    # # Guardamos el dataframe en un csv
+    # submission.to_csv(f'C:/Users/Jose/Documents/UNIVERSIDAD/TFG/MATEMATICAS/PYTHON/MODELOS/XGBoost_{current_time}/submission_lastob_xgb_{fold}.csv', index=False)
+    # print(f'Submission for fold {fold} done')
 
     # Liberamos memoria
     del X_train, X_valid, y_train, y_valid, dtrain, dvalid
